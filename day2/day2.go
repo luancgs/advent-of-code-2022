1098192a2ffa903a0e9cf387c07d0621ad5f3c0a
Luan Carlos <luan.carlos.3174@gmail.com>
2022-12-02 13:32:36 -0300

    feat: :sparkles: adds day 2 solution

--- day2/day2.go
+++ day2/day2.go
@@ -1,11 +1,174 @@
+/*
+	Day 2: Rock Paper Scissors
+	Link: https://adventofcode.com/2022/day/2
+*/
+
 package day2
 
-import "fmt"
+import (
+	"os"
+	"strings"
+)
 
-func Part1() {
-	fmt.Println("Not implemented")
+var possibleResult = map[string][]string{
+	"rock":     {"paper", "scissors"},
+	"paper":    {"scissors", "rock"},
+	"scissors": {"rock", "paper"},
 }
 
-func Part2() {
-	fmt.Println("Not implemented")
+var resultPoints = map[string]int{
+	"win":  6,
+	"draw": 3,
+	"lose": 0,
+}
+
+var inputPoints = map[string]int{
+	"rock":     1,
+	"paper":    2,
+	"scissors": 3,
+}
+
+var opponentInputs = map[string]string{
+	"A": "rock",
+	"B": "paper",
+	"C": "scissors",
+}
+
+var myInputsPart1 = map[string]string{
+	"X": "rock",
+	"Y": "paper",
+	"Z": "scissors",
+}
+
+var myInputsPart2 = map[string]string{
+	"X": "lose",
+	"Y": "draw",
+	"Z": "win",
+}
+
+type gameResult struct {
+	name   string
+	points int
+}
+
+type gameRound struct {
+	opponentInput string
+	myInput       string
+}
+
+func (round gameRound) getResult() gameResult {
+	var result string
+
+	if round.opponentInput == round.myInput {
+		result = "draw"
+	} else if (round.opponentInput == "rock" && round.myInput == "paper") || (round.opponentInput == "paper" && round.myInput == "scissors") || (round.opponentInput == "scissors" && round.myInput == "rock") {
+		result = "win"
+	} else {
+		result = "lose"
+	}
+
+	return gameResult{result, resultPoints[result]}
+}
+
+func (round gameRound) getScore() int {
+	scoreResult := round.getResult().points
+	scoreInput := inputPoints[round.myInput]
+
+	return scoreInput + scoreResult
+}
+
+type gameRoundPart1 struct {
+	gameRound
+}
+
+type gameRoundPart2 struct {
+	gameRound
+}
+
+func (round *gameRoundPart1) convertInputs() {
+	round.opponentInput = opponentInputs[round.opponentInput]
+	round.myInput = myInputsPart1[round.myInput]
+}
+
+func (round *gameRoundPart2) convertInputs() {
+	round.opponentInput = opponentInputs[round.opponentInput]
+
+	if myInputsPart2[round.myInput] == "win" {
+		round.myInput = possibleResult[round.opponentInput][0]
+	} else if myInputsPart2[round.myInput] == "lose" {
+		round.myInput = possibleResult[round.opponentInput][1]
+	} else {
+		round.myInput = round.opponentInput
+	}
+}
+
+func newRoundPart1(opponentInput string, myInput string) gameRoundPart1 {
+	round := gameRoundPart1{gameRound{opponentInput, myInput}}
+	round.convertInputs()
+	return round
+}
+
+func newRoundPart2(opponentInput string, myInput string) gameRoundPart2 {
+	round := gameRoundPart2{gameRound{opponentInput, myInput}}
+	round.convertInputs()
+	return round
+}
+
+func getInput() ([][]string, error) {
+	inputPath := "./day2/input.txt"
+
+	content, err := os.ReadFile(inputPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var output [][]string
+
+	roundsArray := strings.Split(string(content), "\n")
+
+	for _, element := range roundsArray {
+
+		roundInputsArray := strings.Split(element, " ")
+
+		if len(roundInputsArray) < 2 {
+			continue
+		}
+
+		output = append(output, roundInputsArray)
+	}
+
+	return output, nil
+}
+
+func Part1() (int, error) {
+	input, err := getInput()
+
+	if err != nil {
+		return 0, err
+	}
+
+	myScore := 0
+
+	for _, round := range input {
+		myScore += newRoundPart1(round[0], round[1]).getScore()
+	}
+
+	return myScore, nil
+}
+
+func Part2() (int, error) {
+	input, err := getInput()
+
+	if err != nil {
+		return 0, err
+	}
+
+	myScore := 0
+
+	for _, round := range input {
+		myScore += newRoundPart2(round[0], round[1]).getScore()
+	}
+
+	return myScore, nil
 }
--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"adventofcode2022/day1"
+	"adventofcode2022/day2"
 	"fmt"
 )
 
 func main() {
 
-	output1, err := day1.Part1()
+	output1, err := day2.Part1()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Day 1 - Part 1 | Output: ", output1)
+	fmt.Println("Day 2 - Part 1 | Output: ", output1)
 
-	output2, err := day1.Part2()
+	output2, err := day2.Part2()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Day 1 - Part 2 | Output: ", output2)
+	fmt.Println("Day 2 - Part 2 | Output: ", output2)
 
 }
