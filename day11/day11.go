4c4c710f8fdb59bff8de0d840599e5db8b0121b0
Luan Carlos <luan.carlos.3174@gmail.com>
2022-12-14 21:31:02 -0300

    feat: 💀 adds day 11 solution

---
+++ day11/day11.go
@@ -0,0 +1,213 @@
+/*
+	Day 11: Monkey in the Middle
+	Link: https://adventofcode.com/2022/day/11
+*/
+
+package day11
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type item struct {
+	worryLevel int
+}
+
+type monkey struct {
+	startingItems []item
+	operation     []string
+	test          int
+	ifTrue        int
+	ifFalse       int
+	inspected     int
+}
+
+func (m monkey) operationWorryLevel(item item) (int, error) {
+	num, err := strconv.Atoi(m.operation[1])
+
+	if err != nil {
+		return 0, err
+	}
+
+	if num == -1 {
+		num = item.worryLevel
+	}
+
+	switch m.operation[0] {
+	case "+":
+		return item.worryLevel + num, nil
+	case "-":
+		return item.worryLevel - num, nil
+	case "*":
+		return item.worryLevel * num, nil
+	case "/":
+		return item.worryLevel / num, nil
+	default:
+		return 0, fmt.Errorf("error calculating operation in worry level")
+	}
+}
+
+func throwItems(monkey int, monkeysPointer *[]monkey, reliefFactor bool) error {
+	monkeys := *monkeysPointer
+
+	for _, monkeyItem := range monkeys[monkey].startingItems {
+		monkeys[monkey].inspected++
+
+		worryLevel, err := monkeys[monkey].operationWorryLevel(monkeyItem)
+
+		if err != nil {
+			return err
+		}
+
+		if reliefFactor {
+			worryLevel = int(math.Floor(float64(worryLevel) / 3))
+		}
+
+		monk := monkeys[monkey]
+
+		if !reliefFactor {
+			var module int = 1
+			for _, mk := range monkeys {
+				module *= mk.test
+			}
+
+			worryLevel %= module
+		}
+
+		if worryLevel%monkeys[monkey].test == 0 {
+			monkeys[monk.ifTrue].startingItems = append(monkeys[monk.ifTrue].startingItems, item{worryLevel})
+		} else {
+			monkeys[monk.ifFalse].startingItems = append(monkeys[monk.ifFalse].startingItems, item{worryLevel})
+		}
+	}
+
+	monkeys[monkey].startingItems = []item{}
+
+	return nil
+}
+
+func getInput() ([]monkey, error) {
+	inputPath := "./day11/input.txt"
+
+	input, err := os.ReadFile(inputPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	inputMonkeys := strings.Split(string(input), "\n\n")
+
+	var monkeys []monkey
+
+	for _, inputMonkey := range inputMonkeys {
+		inputMonkeyLines := strings.Split(inputMonkey, "\n")
+
+		startingItemsString := strings.Split(strings.Trim(strings.ReplaceAll(inputMonkeyLines[1], "Starting items: ", ""), " "), ", ")
+		var startingItems []item
+
+		for _, itemString := range startingItemsString {
+			num, err := strconv.Atoi(itemString)
+
+			if err != nil {
+				return nil, err
+			}
+
+			startingItems = append(startingItems, item{num})
+		}
+
+		operation := strings.Split(strings.Trim(strings.ReplaceAll(inputMonkeyLines[2], "Operation: new = old ", ""), " "), " ")
+
+		if operation[1] == "old" {
+			operation[1] = "-1"
+		}
+
+		testString := strings.Trim(strings.ReplaceAll(inputMonkeyLines[3], "Test: divisible by ", ""), " ")
+
+		test, err := strconv.Atoi(testString)
+
+		if err != nil {
+			return nil, err
+		}
+
+		ifTrueString := strings.Trim(strings.ReplaceAll(inputMonkeyLines[4], "If true: throw to monkey ", ""), " ")
+
+		ifTrue, err := strconv.Atoi(ifTrueString)
+
+		if err != nil {
+			return nil, err
+		}
+
+		ifFalseString := strings.Trim(strings.ReplaceAll(inputMonkeyLines[5], "If false: throw to monkey ", ""), " ")
+
+		ifFalse, err := strconv.Atoi(ifFalseString)
+
+		if err != nil {
+			return nil, err
+		}
+
+		monkeys = append(monkeys, monkey{startingItems, operation, test, ifTrue, ifFalse, 0})
+	}
+
+	return monkeys, nil
+
+}
+
+func Part1() (int, error) {
+	monkeys, err := getInput()
+
+	if err != nil {
+		return 0, err
+	}
+
+	for round := 1; round <= 20; round++ {
+		for monkey := 0; monkey < len(monkeys); monkey++ {
+			throwItems(monkey, &monkeys, true)
+		}
+	}
+
+	var monkeyInspects []int
+
+	for _, monkey := range monkeys {
+		monkeyInspects = append(monkeyInspects, monkey.inspected)
+	}
+
+	sort.Ints(monkeyInspects)
+	monkeyInspects = monkeyInspects[len(monkeyInspects)-2:]
+
+	return monkeyInspects[0] * monkeyInspects[1], nil
+}
+
+func Part2() (int, error) {
+	monkeys, err := getInput()
+
+	if err != nil {
+		return 0, err
+	}
+
+	var module int = 1
+	for _, mk := range monkeys {
+		module *= mk.test
+	}
+
+	for round := 1; round <= 10000; round++ {
+		for monkey := 0; monkey < len(monkeys); monkey++ {
+			throwItems(monkey, &monkeys, false)
+		}
+	}
+
+	var monkeyInspects []int
+
+	for _, monkey := range monkeys {
+		monkeyInspects = append(monkeyInspects, monkey.inspected)
+	}
+
+	sort.Ints(monkeyInspects)
+	monkeyInspects = monkeyInspects[len(monkeyInspects)-2:]
+
+	return monkeyInspects[0] * monkeyInspects[1], nil
+}
--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"adventofcode2022/day10"
+	"adventofcode2022/day11"
 	"fmt"
 )
 
 func main() {
 
-	output1, err := day10.Part1()
+	output1, err := day11.Part1()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Day 10 - Part 1 | Output: ", output1)
+	fmt.Println("Day 11 - Part 1 | Output: ", output1)
 
-	output2, err := day10.Part2()
+	output2, err := day11.Part2()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Day 10 - Part 2 | Output: ", output2)
+	fmt.Println("Day 11 - Part 2 | Output: ", output2)
 
 }
