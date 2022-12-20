993ef8c95da6a38f7d0722b525753cfd747408e7
Luan <luan.carlos.3174@gmail.com>
2022-12-20 00:49:36 -0300

    chore: :sparkles: adds sample input files to input package

--- utils/input/input.go
+++ utils/input/input.go
@@ -6,8 +6,15 @@ import (
 )
 
 // Receives the day and returns the input as a string
-func GetInput(day int) string {
-	inputPath := fmt.Sprintf("./inputs/2022/day%02d.txt", day)
+func GetInput(day int, isTest ...bool) string {
+	var inputPath string
+
+	if len(isTest) > 0 && isTest[0] {
+		inputPath = fmt.Sprintf("./inputs/2022/sample/day%02d.txt", day)
+	} else {
+		inputPath = fmt.Sprintf("./inputs/2022/day%02d.txt", day)
+	}
+
 	content, err := os.ReadFile(inputPath)
 
 	if err != nil {
