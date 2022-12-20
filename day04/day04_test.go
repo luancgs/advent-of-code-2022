4be69505ef755b6dc214dc7437ea47be95e05828
Luan <luan.carlos.3174@gmail.com>
2022-12-20 12:14:55 -0300

    test: :white_check_mark: adds day 4 sample input test

---
+++ day04/day04_test.go
@@ -0,0 +1,42 @@
+package day04
+
+import (
+	"adventofcode2022/utils/input"
+	"testing"
+)
+
+type SampleReader struct{}
+
+func (SampleReader) GetInput(day int, isTest ...bool) string {
+	return input.Reader.GetInput(input.Reader{}, 4, true)
+}
+
+func TestPart1(t *testing.T) {
+	expected := 2
+	got, err := Part1(SampleReader{})
+
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	if expected != got {
+		t.Logf("Expected %v but got %v", expected, got)
+		t.Fail()
+	}
+}
+
+func TestPart2(t *testing.T) {
+	expected := 4
+	got, err := Part2(SampleReader{})
+
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	if expected != got {
+		t.Logf("Expected %v but got %v", expected, got)
+		t.Fail()
+	}
+}
